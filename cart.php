ddc3bccf996d7ee1ac2789355f162d64cf023f7d
Jian Yi <1211311173@student.mmu.edu.my>
2025-05-14 14:08:46 +0800

    Add in new dependencies

--- cart.php
+++ cart.php
@@ -1,10 +1,8 @@
 <?php include_once('./includes/headerNav.php'); ?>
-
 <div class="overlay" data-overlay></div>
 <!--
-    - HEADER
+  - HEADER
   -->
-
 <header>
   <!-- top head action, search etc in php -->
   <!-- inc/topheadactions.php -->
@@ -15,29 +13,28 @@
   <!-- mobile nav in php -->
   <!-- inc/mobilenav.php -->
   <?php require_once './includes/mobilenav.php'; ?>
-
 <!-- style -->
-    <style>
-      :root{
-         --main-maroon: #CE5959;
-        --deep-maroon: #89375F;
-      }
-      td,th{
-        text-align:center;
-      }
-      td img{
-        margin-left:auto;
-        margin-right:auto;
-      }
-      .delete-icon{
-        color:var(--bittersweet);     
-        cursor: pointer; 
-      }
-  .child-register-btn {
-    margin-top:20px;
-    width:85%;
+  <style>
+    :root{
+     --main-maroon: #CE5959;
+    --deep-maroon: #89375F;
+    }
+    td,th{
+    text-align:center;
+    }
+    td img{
     margin-left:auto;
     margin-right:auto;
+    }
+    .delete-icon{
+    color:var(--bittersweet);     
+    cursor: pointer; 
+    }
+  .child-register-btn {
+  margin-top:20px;
+  width:85%;
+  margin-left:auto;
+  margin-right:auto;
 }
 .child-register-btn p {
   width: 350px;
@@ -54,106 +51,69 @@
   font-weight: 600;
 }
 @media screen and (max-width: 794px) {
- 
   .child-register-btn {
-    margin-top:30px;
-  
+  margin-top:30px;
 }
  .child-register-btn p {
    width: 100%;
  }
 }
-
-    </style>
+  </style>
 </header>
-
-
-
-
-
 <!--
-    - MAIN
+  - MAIN
   -->
-
 <main>
-
-    <div class="product-container">
-        <div class="container">
-            <!--
-                - SIDEBAR
-           -->
-
-
-           <table>
+  <div class="product-container">
+    <div class="container">
+      <!--
+        - SIDEBAR
+       -->
+       <table>
   <tr>
-    <th>Image</th>
-    <th>Name</th>
-    <th>Price</th>
-    <th>Quantity</th>
+  <th>Image</th>
+  <th>Name</th>
+  <th>Price</th>
+  <th>Quantity</th>
   </tr>
   <?php
-
   if(isset($_SESSION['mycart']))
   {
-    foreach($_SESSION['mycart'] as $value)
-    {
-  
-  
-    ?>
-    <tr>
-      <td>
-      <img class="cart-product-image" src="./admin/upload/<?php echo $value['product_img'] ?>"  alt="">
-      </td>
-      <td><?php echo  $value['name']; ?></td>
-      <td><?php echo  "$".$value['price']; ?></td>
-      <td ><?php echo $value['product_qty']; ?></td>
-    </tr>
-  
-    <?php
-    }}else
-    {
-
-    
-  
-    ?>
-        <tr >
-      <td colspan='4' >No item available in cart</td>
-    </tr>
-
-    <?php
-    }
-
-    ?>
-
-  
- 
-
+  foreach($_SESSION['mycart'] as $value)
+  {
+  ?>
+  <tr>
+    <td>
+    <img class="cart-product-image" src="./admin/upload/<?php echo $value['product_img'] ?>"  alt="">
+    </td>
+    <td><?php echo  $value['name']; ?></td>
+    <td><?php echo  "$".$value['price']; ?></td>
+    <td ><?php echo $value['product_qty']; ?></td>
+  </tr>
+  <?php
+  }}else
+  {
+  ?>
+    <tr >
+    <td colspan='4' >No item available in cart</td>
+  </tr>
+  <?php
+  }
+  ?>
 </table>
-
-
-     
-     </div>
-
-           
-        </div>
+   </div>
     </div>
-
-    <?php
-
+  </div>
+  <?php
 if(isset($_SESSION['mycart']))
-            { 
+      { 
    ?>
-    <div class="child-register-btn">
-                               
-                               <p > <a href="checkout.php" style="color:#FFFFFF">Proceed To CheckOut</a>
-                                </p>
-                           </div>
-
-                           <?php
+  <div class="child-register-btn">
+                 <p > <a href="checkout.php" style="color:#FFFFFF">Proceed To CheckOut</a>
+                </p>
+               </div>
+               <?php
 }
-
-                           ?>
+               ?>
 </main>
-
-
 <?php require_once './includes/footer.php'; ?>
